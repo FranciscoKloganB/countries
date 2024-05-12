4babdef1025b6278a41517cdc691ecc6e9457c15
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2024-05-12 13:08:11 +0100

    fix(package.json): update release script

--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,15 @@
 // With TypeScript `esModuleInterop` the two statements below are equivalent;
 // const utilsCJS = require("@franciscokloganb/countries");
-import * as CountriesAliasPKG from '@franciscokloganb/countries'
+import * as CountriesAliasPKG from '@franciscokloganb/countries';
 // Destruct MJS import
-import { countries } from '@franciscokloganb/countries'
+import { countries } from '@franciscokloganb/countries';
 // Default MJS export
-import CountriesDefaultPKG from '@franciscokloganb/countries'
+import CountriesDefaultPKG from '@franciscokloganb/countries';
 // console.log("MJS Default Import:", pkg, pkg.toHumanReadableString("2022"));
 
-console.log('CJS Require Import:', typeof CountriesAliasPKG.countries !== 'undefined')
-console.log('MJS Destructured Import:', typeof countries !== 'undefined')
-console.log('MJS Default Import:', typeof CountriesDefaultPKG.countries !== 'undefined')
+console.log('CJS Require Import:', typeof CountriesAliasPKG.countries !== 'undefined');
+console.log('MJS Destructured Import:', typeof countries !== 'undefined');
+console.log('MJS Default Import:', typeof CountriesDefaultPKG.countries !== 'undefined');
 
 /**
  * This file serves only to test your publishable NPM package various import
@@ -18,14 +18,14 @@ console.log('MJS Default Import:', typeof CountriesDefaultPKG.countries !== 'und
  * Use `npm link` and try to import your stuff in this file.
  */
 
-const assets = 'node_modules/@franciscokloganb/countries/assets'
+const assets = 'node_modules/@franciscokloganb/countries/assets';
 
 function ipath(c: string) {
-  return new URL(`${assets}/${c.toLowerCase()}.svg`, import.meta.url).href
+  return new URL(`${assets}/${c.toLowerCase()}.svg`, import.meta.url).href;
 }
 
 const FlagsSVG = {
   portugal: ipath('pt'),
-} as const
+} as const;
 
-export { FlagsSVG }
+export { FlagsSVG };
